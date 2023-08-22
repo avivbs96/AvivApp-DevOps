4133c3fef3ee185aea424ab6957ce9c2b8226e06
avivbs96 <85108235+avivbs96@users.noreply.github.com>
2023-08-22 15:59:25 +0300

    update script

--- selenium_script.py
+++ selenium_script.py
@@ -1,40 +1,50 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 import time
 
-time.sleep(20)
 
 try:
     # Initialize the driver
-    service = Service(executable_path='/usr/local/bin/chromedriver')
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+    print("Driver initialized.")
 
     # Navigate to the page
     driver.get("http://localhost:8081/Aviv-tomcat/AvivApp.jsp")
+    print("Navigated to the application.")
 
     # Verification 1: Check Title
-    assert "Aviv JSP App" in driver.title
+    if "Aviv JSP App" in driver.title:
+        print("Verification 1: Title check passed.")
+    else:
+        print("Verification 1: Title check failed.")
 
     # Verification 2: Check GitHub Button
     try:
-        button = driver.find_element(By.TAG_NAME, "button")
-        assert "Go to Git Hub" in button.text
+        button = driver.find_element_by_tag_name("button")
+        if "Go to Git Hub" in button.text:
+            print("Verification 2: GitHub button check passed.")
+        else:
+            print("Verification 2: GitHub button check failed.")
     except NoSuchElementException:
-        print("Button not found")
+        print("Verification 2: GitHub button not found.")
 
     # Verification 3: Check Header
     try:
-        header = driver.find_element(By.TAG_NAME, "h1")
-        assert "Welcome to Aviv Ben Shitrit Git Hub Page!" == header.text
+        header = driver.find_element_by_tag_name("h1")
+        if "Welcome to Aviv Ben Shitrit Git Hub Page!" == header.text:
+            print("Verification 3: Header check passed.")
+        else:
+            print("Verification 3: Header check failed.")
     except NoSuchElementException:
-        print("Header not found")
+        print("Verification 3: Header not found.")
 
 except Exception as e:
     print(f"An error occurred: {e}")
 
 finally:
     # Close the browser
+    time.sleep(20)
+
     driver.quit()
+    print("Browser closed.")
