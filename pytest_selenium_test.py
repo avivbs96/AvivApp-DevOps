778bb9530711eb2d26a3eec8f3e4255a82678553
avivbs96 <85108235+avivbs96@users.noreply.github.com>
2023-08-22 16:23:01 +0300

    update script

--- pytest_selenium_test.py
+++ pytest_selenium_test.py
@@ -1,9 +1,9 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.service import Service
-
 
 @pytest.fixture(scope="module")
 def setup_driver():
@@ -26,11 +26,11 @@ def test_github_button(setup_driver):
         print("Verification 2: GitHub button check passed.")
     except NoSuchElementException:
         pytest.fail("Button not found")
-        
+
 def test_header(setup_driver):
     driver = setup_driver
     try:
-        header = driver.find_element_by_tag_name("h1")
+        header = driver.find_element(By.TAG_NAME, "h1")
         assert "Welcome to Aviv Ben Shitrit Git Hub Page!" == header.text
         print("Verification 3: Header check passed.")
     except NoSuchElementException:
